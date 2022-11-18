5c12d2b4979a8b55438f2ca39ebefce4ac9b52ac
Akash Ingale <92642182+bluesky62@users.noreply.github.com>
2022-11-18 15:23:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('The Avenger');
+  // console.log(searchValue);
+
+  const getMovieRequest = async () => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  });
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row main">
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
